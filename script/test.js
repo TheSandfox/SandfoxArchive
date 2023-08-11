d30065113497142ef9b93a22aa74b8e0619d3633
TheSandfox <sandfox9411@gmail.com>
2023-08-11 23:28:44 +0900

    jsontest

--- script/test.js
+++ script/test.js
@@ -1,3 +1,14 @@
-import fs from 'fs'
-var jsn = JSON.parse(fs.readFileSync('extraw3x/SkillArchive/json/Ability/Ability0100.json'))
-document.getElementById("jsonOutPut").innerHTML = jsn["TOOLTIP"]
+
+fetch('extraw3x/SkillArchive/json/Ability/Ability0100.json')
+.then(response => response.json())
+.then(data => {
+	const outputDiv = document.getElementById('jsonOutPut');
+	outputDiv.innerHTML = data["TOOLTIP"]
+})
+.catch(error => {
+	console.error('Error reading or parsing JSON file:', error);
+});
+
+/*var reader = new FileReader();
+var jsn = JSON.parse(reader.readAsText())
+document.getElementById("jsonOutPut").innerHTML = jsn["TOOLTIP"]*/
