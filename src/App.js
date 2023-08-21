dfaef93370e6a2af732656456198151aec0117f6
TheSandfox <sandfox9411@gmail.com>
2023-08-22 00:08:18 +0900

    빌드커맨드 추가하고 다시

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //import react
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom';
 
 //import component
 import TopBar from './component/TopBar';
@@ -21,6 +21,7 @@ import './css/ability-description.css'
 //RefineAbilityJSON
 const AbilityJson = []
 let i = 0
+let tempstring = '{'
 while(i < AbilityParams["params"].length) {
 	let originjson = AbilityParams["params"][i]
 	let abilid = originjson["ID"]
@@ -29,8 +30,16 @@ while(i < AbilityParams["params"].length) {
 		newjson["TOOLTIP"] = AbilityTooltips[abilid]["TOOLTIP"]
 	}
 	AbilityJson.push(newjson)
+	//
+	if(i==0) {
+		tempstring = tempstring+'"'+originjson["ID"]+'":"'+String(i)+'"'
+	} else {
+		tempstring = tempstring+',"'+originjson["ID"]+'":"'+String(i)+'"'
+	}
 	i++
 }
+tempstring = tempstring + '}'
+const AbilityMap = JSON.parse(tempstring)
 
 function App() {
 
@@ -41,6 +50,7 @@ function App() {
 			<div className="mid">
 			<Routes>
 				<Route exact path="/w3x/SkillArchive/Ability" element={<AbilityDescriptionContainer/>}/>
+				<Route path="/w3x/SkillArchive/Ability/:id" element={<AbilityDescription/>}/>
 			</Routes>
 			</div>
 			<div className="bottom-bar">
@@ -52,23 +62,37 @@ function App() {
 	);
 }
 
+//어빌리티 설명박스
 function AbilityDescription(props) {
-	let abiljson = props.json
-	let stl = {
-		border:'2px solid #'+CustomString[abiljson["TIER"]]["COLOR"]
+	let abiljson = {}
+	let p = useParams()
+	if (props.json===undefined) {
+		//제이슨 없이 param으로 라우팅해서 컴포넨트 호출
+		abiljson = AbilityJson[AbilityMap[p.id]]
+	} else {
+		//제이슨 줘서 컴포넨트 호출
+		abiljson = props.json
 	}
-	return <div className="ability-description-container" style={stl}>
-		<div className="top">
-			<img src={process.env.PUBLIC_URL+"/resource/"+abiljson.ICON_PATH} alt={process.env.PUBLIC_URL+"/resource/replaceabletextures/commandbuttons/btncancle.png"}/>
-			<div>
-				<div className="ability-name">#{abiljson.ID} {abiljson.NAME}</div>
-				<div className="ability-tags">{abiljson.TAGS}</div>
+	
+	if (abiljson!==undefined) {
+		let stl = {
+			border:'2px solid #'+CustomString[abiljson["TIER"]]["COLOR"]
+		}
+		return <div className="ability-description-container" style={stl}>
+			<div className="top">
+				<img src={process.env.PUBLIC_URL+"/resource/"+abiljson.ICON_PATH} alt={process.env.PUBLIC_URL+"/resource/replaceabletextures/commandbuttons/btncancle.png"}/>
+				<div>
+					<div className="ability-name">#{abiljson.ID} {abiljson.NAME}</div>
+					<div className="ability-tags">{abiljson.TAGS}</div>
+				</div>
+			</div>
+			<div className="bottom">
+				<div dangerouslySetInnerHTML={{__html:abiljson.TOOLTIP}}></div>
 			</div>
 		</div>
-		<div className="bottom">
-			<div dangerouslySetInnerHTML={{__html:abiljson.TOOLTIP}}></div>
-		</div>
-	</div>
+	} else {
+		return <></>
+	}
 }
 
 function AbilityDescriptionContainer() {
