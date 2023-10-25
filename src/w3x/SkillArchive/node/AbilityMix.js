51316d0856219a14eeed65a7373ccee2c052a9ba
TheSandfox <sandfox9411@gmail.com>
2023-10-26 01:59:29 +0900

    231026

--- src/w3x/SkillArchive/node/AbilityMix.js
+++ src/w3x/SkillArchive/node/AbilityMix.js
@@ -8,10 +8,8 @@ function main() {
 		var outJass = 'C:/war3lib/maps/SkillArchive/Ability/GeneratedAbilityMix.j'
 		var i = 1;/*두 번째 행부터*/
 		var data1 	= 0	/*main_id*/
-		var count1 	= 2	/*main_count*/
-		var data2 	= 4	/*sub_id*/
-		var count2 	= 6	/*sub_count*/
-		var data3 	= 8	/*result*/
+		var data2 	= 3	/*sub_id*/
+		var data3 	= 6	/*result*/
 		fs.writeFileSync(outJson, '{"params":[','utf-8');
 		fs.writeFileSync(outJass,'','utf-8');
 		while (true) {
@@ -23,7 +21,7 @@ function main() {
 			fs.appendFileSync(outJson,'\n\t\t"ID2":"'+rows[i][data2]+'",','utf-8')
 			fs.appendFileSync(outJson,'\n\t\t"RESULT":"'+rows[i][data3]+'"\n\t}','utf-8')
 			/*JASS */
-			fs.appendFileSync(outJass,"call AbilityMix.create('"+rows[i][data1]+"', '"+rows[i][data2]+"', "+rows[i][count1]+", "+rows[i][count2]+", '"+rows[i][data3]+"')\n",'utf-8')
+			fs.appendFileSync(outJass,"call AbilityMix.create('"+rows[i][data1]+"', '"+rows[i][data2]+"', '"+rows[i][data3]+"')\n",'utf-8')
 			i++
 		}
 		fs.appendFileSync(outJson, "\n]}",'utf-8');
