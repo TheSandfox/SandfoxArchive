aef9ba463b38df023424144b1efe0a06f28572ef
nowon <sandfox9411@gmail.com>
2024-02-23 17:50:03 +0900

    240223

--- src/component/w3x/SkillArchive/SkillArchive.js
+++ src/component/w3x/SkillArchive/SkillArchive.js
@@ -5,24 +5,26 @@ import Nav from "component/Nav";
 
 export default function SkillArchive() {
 	return <>
-	<Nav form={[
-		{
-			route:"/w3x/SkillArchive/Ability",
-			img:"BTNSpellBookBLS.png",
-			title:"스킬 정보"
-		},
-		{
-			route:"/w3x/SkillArchive/Unit",
-			img:"btnfootman.png",
-			title:"유닛 정보"
-		}
-	]}/>
-	<Routes>
-		<Route exact path="/" element={<div>K-스사막 소개페이지</div>}/>
-		<Route exact path="/Ability" element={<Ability/>}/>
-		<Route path="/Ability/:id" element={<Ability isSingle={true}/>}/>
-		<Route exact path="/Unit" element={<Unit/>}/>
-		<Route path="/Unit/:id" element={<Unit isSingle={true}/>}/>
-	</Routes>
+		<Nav form={[
+			{
+				route:"/w3x/SkillArchive/Ability",
+				img:"BTNSpellBookBLS.png",
+				title:"스킬 정보"
+			},
+			{
+				route:"/w3x/SkillArchive/Unit",
+				img:"btnfootman.png",
+				title:"유닛 정보"
+			}
+		]}/>
+		<div className="mid">
+			<Routes>
+				<Route exact path="/" element={<div>K-스사막 소개페이지</div>}/>
+				<Route exact path="/Ability" element={<Ability/>}/>
+				<Route path="/Ability/:id" element={<Ability isSingle={true}/>}/>
+				<Route exact path="/Unit" element={<Unit/>}/>
+				<Route path="/Unit/:id" element={<Unit isSingle={true}/>}/>
+			</Routes>
+		</div>
 	</>
 }
