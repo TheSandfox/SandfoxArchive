bd97136e818da57ea3b1f0c45bdf467b4eb59cb3
thesandfox <sandfox9411@gmail.com>
2024-03-19 17:49:09 +0900

    240319

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,11 +1,20 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import 'css/header.css';
 
+function HeaderRouter({pathName}) {
+	let stringArr = pathName.replace('/','').split('/');
+	console.log(stringArr);
+	return <>
+	</>
+}
+
 export default function Header() {
+	let loc = useLocation();
 	return (
 		<header>
 			<Link to="/"><h1 className="title">SandfoxArchive.info</h1></Link>
+			<HeaderRouter pathName={loc.pathname}/>
 		</header>
 	);
 }
