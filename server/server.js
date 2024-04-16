1cf9ff1316dc7d482b77d9e060ca8eac9bf566cf
TheSandfox <sandfox9411@gmail.com>
2024-04-16 14:36:50 +0900

    240416

--- server/server.js
+++ server/server.js
@@ -4,13 +4,17 @@ const cors = require('cors');
 const router = require('./router');
 const path = require('path');
 
-const port = 3001;
+const port = 3000;
 
 app.use(express.json()); //json관련 기능 사용
 app.use(express.urlencoded({extended:false})); //url인코딩 관련 기능 사용
 app.use('/',router); //라우트 관련 기능 사용
-app.use(express.static(path.join(__dirname,'public'))); //static관련 기능 사용
+app.use(express.static(path.join(__dirname,'build'))); //static관련 기능 사용
 app.use(cors());
+
+app.get('*',(req,res)=>{
+	res.sendFile(path.join(__dirname,'build/index.html'));
+})
 
 app.listen(port,()=>{
 	console.log('SandfoxArchive rest server 가동!');
